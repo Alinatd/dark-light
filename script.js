f19e6e4aec076ddbf2118c73c6835b8a42fa34ca
Alina Turdakunova <141915076+Alinatd@users.noreply.github.com>
2023-09-16 19:37:25 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const con = document.getElementById('con')
+const body = document.body;
+
+const onToggle = ()=>{
+body.classList.toggle('bac')
+}
+
+// btn.addEventListener('click',onToggle())
+
+
